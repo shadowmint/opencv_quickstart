dce35d70aca685a60ca95a6d048cd1e1e24ddbf3
unknown <doug@.(none)>
2012-05-03 22:51:44 +0800

    Made opencv test work

--- tests/opencv_works/Test.cpp
+++ tests/opencv_works/Test.cpp
@@ -1,14 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
-  Mat img(Mat::zeros(100, 100, CV_8U));
+  Mat img;
 
-  imshow("window", img);
+  img = imread("sample.png", CV_LOAD_IMAGE_COLOR);
+  if (!img.data) {
+    cout << "Unable to open image" << std::endl;
+  }
 
   return 0;
 }
